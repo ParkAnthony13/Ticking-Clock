97cfaa50d8c026b610a53cb480d5da705151c71b
ParkAnthony13 <anth0nypark13@gmail.com>
2021-05-27 10:31:23 -0700

    Added toggle functionality, 12 to 24 hour clock

--- script.js
+++ script.js
@@ -3,14 +3,26 @@ function getSecondsSinceStartOfDay() {
     new Date().getMinutes() * 60 + 
     new Date().getHours() * 3600;
 }
+var toggle = document.querySelector('.cycle').innerHTML;
+function changeClock(element) {
+    if (toggle == "12 Hour Clock") {
+        toggle = "24 Hour Clock";
+        console.log(toggle);
+        element.innerHTML = "24 Hour Clock";
+    } else if (toggle == "24 Hour Clock") {
+        toggle = "12 Hour Clock";
+        console.log(toggle);
+        element.innerHTML = "12 Hour Clock";
+    }
+}
 
 setInterval( function() {
     var time = getSecondsSinceStartOfDay();
     // console.log(time);
     var hour = new Date().getHours()+30;
-    // console.log(hour);
+    console.log(hour);
     var min = new Date().getMinutes()+30;
-    console.log(min);
+    // console.log(min);
     var sec = new Date().getSeconds()+30;
     // console.log(sec);
 
@@ -21,10 +33,26 @@ setInterval( function() {
     var sHour = hour.toString();
     var sMin = min.toString();
     var sSec = (sec-(hour*3600+min*60)).toString();
-    if (sHour.length < 2) {
-        document.querySelector(".hr").innerHTML = "0"+hour-30;
-    } else {
-        document.querySelector(".hr").innerHTML = hour-30;
+    var twoCycle = hour - 30;
+    console.log(twoCycle);
+    if (toggle == "12 Hour Clock") {
+        if (twoCycle > 12) {
+            twoCycle = twoCycle-12;
+            twoCycle = twoCycle.toString();
+        } else {
+            twoCycle = twoCycle.toString();
+        }
+        if (twoCycle.length < 2) {
+            document.querySelector(".hr").innerHTML = "0"+ twoCycle;
+        } else {
+            document.querySelector(".hr").innerHTML = twoCycle;
+        }
+    } else if (toggle == "24 Hour Clock") {
+        if (twoCycle.length < 2) {
+            document.querySelector(".hr").innerHTML = "0"+ twoCycle;
+        } else {
+            document.querySelector(".hr").innerHTML = twoCycle;
+        }
     }
     if (min < 40) {
         document.querySelector(".min").innerHTML = "0"+min-30;
@@ -38,9 +66,14 @@ setInterval( function() {
     }
 }, 1000);
 
+// if (twoCycle < 2) {
+//     document.querySelector(".hr").innerHTML = "0"+hour-30;
+// } else {
+//     document.querySelector(".hr").innerHTML = hour-30;
+
 setInterval( function() {
     var milisecond = new Date().getMilliseconds();
-    console.log(milisecond);
+    // console.log(milisecond);
     var sMilisecond = milisecond.toString();
     if (sMilisecond == 2) {
         document.querySelector(".mili").innerHTML = "0" + milisecond;
